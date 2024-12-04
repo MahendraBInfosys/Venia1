b8790573eb621204ba310b113be6bbe08f39a94a
MahendraBInfosys <mahendra.bod@infosys.com>
2024-12-03 22:52:57 -0800

    added css for hr below category name

--- src/Banner.js
+++ src/Banner.js
@@ -4,6 +4,7 @@ function Banner() {
     <div className="banner">
       <div className="banner-title-section">
         <span>Men's Outewear</span>
+        <hr />
       </div>
       <div className="banner-img-section">
         <img width="100%" height="350px" src="./pexels-solliefoto-298863.jpg" alt="Men's Outerwear" />
